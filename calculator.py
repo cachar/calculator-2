a9e55fe56f5b68a5c06db3d935b8648a808c9447
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 15:55:35 -0800

    removed redundancy in declaring function symbols

--- calculator.py
+++ calculator.py
@@ -6,7 +6,6 @@ calculator program yourself in this file.
 """
 
 from arithmetic import *
-math_functions = ['+', '-', '*', '/', '%', '**2', "**3", "**", "square", "cube", "mod", "pow"]
 user_entry = raw_input("Please type an arithmetic function followed by 2 numbers. ")
 # Your code goes here
 while True:
@@ -17,25 +16,23 @@ while True:
 
     if response[0] == "q" or response[0]== "Q":
         break
-    elif response[0] in math_functions:
-            if response[0] =="+":
-                result = add(num1, num2)
-            elif response[0] =="-":
-                result = subtract(num1, num2)
-            elif response[0] =="*":
-                result = multiply(num1,num2)
-            elif response[0] =="/":
-                result = divide(num1, num2)
-            elif response[0] =="%" or response[0] == "mod":
-                result = mod(num1,num2)
-            elif response[0] =="**2" or response[0] == "square":
-                result = square(num1)
-            elif response[0] =="**3" or response[0] == "cube":
-                result = cube(num1)
-            elif response[0] =="**" or response[0] == "pow":
-                result = power(num1, num2)
-            print "The answer is %.2f" %(result)
-            break
+    elif response[0] =="+":
+        result = add(num1, num2)
+    elif response[0] =="-":
+        result = subtract(num1, num2)
+    elif response[0] =="*":
+        result = multiply(num1,num2)
+    elif response[0] =="/":
+        result = divide(num1, num2)
+    elif response[0] =="%" or response[0] == "mod":
+        result = mod(num1,num2)
+    elif response[0] =="**2" or response[0] == "square":
+        result = square(num1)
+    elif response[0] =="**3" or response[0] == "cube":
+        result = cube(num1)
+    elif response[0] =="**" or response[0] == "pow":
+        result = power(num1, num2)
+        print "The answer is %.2f" %(result)
+        break
     else:
         print "You didn't pick a valid math function."
-        break
