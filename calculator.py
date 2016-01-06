c36a5d9187b7b1c88df723cd27f6c89ab683ffa1
Hackbright Student <info@hackbrightacademy.com>
2016-01-06 15:43:25 -0800

    the actually right version of this calculator 2

--- calculator.py
+++ calculator.py
@@ -11,29 +11,31 @@ user_entry = raw_input("Please type an arithmetic function followed by 2 numbers
 # Your code goes here
 while True:
 
-    user_entry.split(" ")
-    # user_entry[1] = (user_entry[1])
-    # user_entry[2] = (user_entry[2])
-    if user_entry[0] == "q" or user_entry[0]== "Q":
+    response = user_entry.split(" ")
+    num1 = int(response[1])
+    num2 = int(response[2])
+
+    if response[0] == "q" or response[0]== "Q":
         break
-    elif user_entry[0] in math_functions:
-            if user_entry[0] =="+":
-                result = add(user_entry[1], user_entry[2])
-            elif user_entry[0] =="-":
-                result = subtract(user_entry[1], user_entry[2])
-            elif user_entry[0] =="*":
-                result = multiply(user_entry[1],user_entry[2])
-            elif user_entry[0] =="/":
-                result = divide(user_entry[1], user_entry[2])
-            elif user_entry[0] =="%":
-                result = mod(user_entry[1],user_entry[2])
-            elif user_entry[0] =="**2":
-                result = square(user_entry[1])
-            elif user_entry[0] =="**3":
-                result = cube(user_entry[1])
-            elif user_entry[0] =="**":
-                result = power(user_entry[1], user_entry[2])
+    elif response[0] in math_functions:
+            if response[0] =="+":
+                result = add(num1, num2)
+            elif response[0] =="-":
+                result = subtract(num1, num2)
+            elif response[0] =="*":
+                result = multiply(num1,num2)
+            elif response[0] =="/":
+                result = divide(num1, num2)
+            elif response[0] =="%":
+                result = mod(num1,num2)
+            elif response[0] =="**2":
+                result = square(num1)
+            elif response[0] =="**3":
+                result = cube(num1)
+            elif response[0] =="**":
+                result = power(num1, num2)
             print "The answer is %.2f" %(result)
+            break
     else:
         print "You didn't pick a valid math function."
         break
